82f2026fb144d8f50c0f5766035a447ff35d4d78
talayounes <tmy07@mail.aub.edu>
2025-04-12 19:44:47 +0300

    Delete models/getroutedoctor.js

--- models/getroutedoctor.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/doctorAvailabilityController');
-
-// GET: Check availability with filters
-router.get('/:doctorId', controller.getAvailability);
-
-module.exports = router;
-
