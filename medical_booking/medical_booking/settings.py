9b110531ac33cf9b0eba66e0d852461115d259d3
jasmienjas <yag05@mail.aub.edu>
2025-04-23 15:32:44 +0300

    add frontend url

--- medical_booking/medical_booking/settings.py
+++ medical_booking/medical_booking/settings.py
@@ -197,6 +197,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+FRONTEND_URL = os.getenv('FRONTEND_URL', 'https://healix-63h5.onrender.com')
 
 
 # Password validation
