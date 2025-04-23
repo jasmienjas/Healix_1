9e2460fe1ea33f0a66a8526fa8de8fc8b5bab59c
jasmienjas <yag05@mail.aub.edu>
2025-04-23 17:54:55 +0300

    debug

---
+++ medical_booking/accounts/migrations/0017_fix_verification_token.py
@@ -0,0 +1,29 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0016_add_birth_date_to_patient_profile'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+            DO $$
+            BEGIN
+                IF NOT EXISTS (
+                    SELECT 1
+                    FROM information_schema.columns
+                    WHERE table_name = 'accounts_customuser'
+                    AND column_name = 'verification_token'
+                ) THEN
+                    ALTER TABLE accounts_customuser ADD COLUMN verification_token VARCHAR(100) NULL;
+                END IF;
+            END $$;
+            """,
+            reverse_sql="""
+            ALTER TABLE accounts_customuser DROP COLUMN IF EXISTS verification_token;
+            """
+        ),
+    ] 
